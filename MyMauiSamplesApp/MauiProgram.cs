76552962d50acd61876a944a4c0a99af7ab7daa3
Zelin Zhu <ZZhu@esri.com>
2025-01-23 20:51:03 -0800

    Added comment

--- MyMauiSamplesApp/MauiProgram.cs
+++ MyMauiSamplesApp/MauiProgram.cs
@@ -24,19 +24,20 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+		// Uncomment the following code to manage the full Android lifecycle and achieve a transparent status bar
 		// #if ANDROID
-		// 		builder.ConfigureLifecycleEvents(events =>
-		// 		{
-		// 			events.AddAndroid(android => android.OnCreate((activity, bundle) =>
-		// 			{
-		// 				var window = activity.Window;
-		// 				if (window is not null)
-		// 				{
-		// 					window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
-		// 					window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
-		// 				}
-		// 			}));
-		// 		});
+		//		builder.ConfigureLifecycleEvents(events =>
+		//		{
+		//			events.AddAndroid(android => android.OnCreate((activity, bundle) =>
+		//			{
+		//				var window = activity.Window;
+		//				if (window is not null)
+		//				{
+		//					window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
+		//					window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+		//				}
+		//			}));
+		//		});
 		// #endif
 
 		RegisterRoutes();
